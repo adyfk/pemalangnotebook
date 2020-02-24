3cc983ab179e07ef49982086b0f4abbc9fa9f152
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2020-02-25 02:01:48 +0700

    success style render first time

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,8 @@ module.exports = {
         stylesProvider: {
           injectFirst: true,
         },
+        disableAutoprefixing: false,
+        disableMinification: false,
       },
     },
     `gatsby-plugin-react-helmet`,
--- plugins/gatsby-plugin-top-layout/gatsby-browser.js
+++ plugins/gatsby-plugin-top-layout/gatsby-browser.js
@@ -5,3 +5,8 @@ import TopLayout from "./TopLayout"
 export const wrapRootElement = ({ element }) => {
   return <TopLayout>{element}</TopLayout>
 }
+// export const wrapPageElement = ({ element, props }) => {
+//   // props provide same data to Layout as Page element will get
+//   // including location, data, etc - you don't need to pass it
+//   return <TopLayout {...props}>{element}</TopLayout>
+// }
