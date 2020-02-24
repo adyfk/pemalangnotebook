f845b6e10f6f9bfcf4915f6737887456d3f3c714
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2020-02-25 00:43:37 +0700

    test init inject new

--- src/components/header/Image/Logo.js
+++ src/components/header/Image/Logo.js
@@ -1,24 +1,20 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const Logo = () => {
   const data = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "logo.png" }) {
         childImageSharp {
-          fluid {
-            src
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
     }
   `)
-  return (
-    <img
-      alt="logo-pemalangnotebook"
-      src={data?.placeholderImage?.childImageSharp?.fluid?.src}
-    />
-  )
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
 export default Logo
