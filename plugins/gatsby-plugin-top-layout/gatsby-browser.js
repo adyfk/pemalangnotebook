aa2fd33f226885282dfdd16089a22fc786ccc6c0
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2020-02-29 08:52:58 +0700

    fix

--- plugins/gatsby-plugin-top-layout/gatsby-browser.js
+++ plugins/gatsby-plugin-top-layout/gatsby-browser.js
@@ -2,11 +2,11 @@
 import React from "react"
 import TopLayout from "./TopLayout"
 
-// export const wrapRootElement = ({ element }) => {
-//   return <TopLayout>{element}</TopLayout>
-// }
-export const wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
-  return <TopLayout {...props}>{element}</TopLayout>
+export const wrapRootElement = ({ element }) => {
+  return <TopLayout>{element}</TopLayout>
 }
+// export const wrapPageElement = ({ element, props }) => {
+//   // props provide same data to Layout as Page element will get
+//   // including location, data, etc - you don't need to pass it
+//   return <TopLayout {...props}>{element}</TopLayout>
+// }
